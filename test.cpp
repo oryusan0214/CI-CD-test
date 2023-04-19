bbe585d59fe21bab417f437e66642d1ea59fe7da
oryusan0214 <yamada.ryu.rhk@gmail.com>
2023-04-19 15:34:38 +0900

    ADD:testコードの追加 C++

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int Increment_Test(int x,int y);
+
+int main (){
+  int i;
+  int count;
+  int anser;
+  
+  count=10;
+  for (i = 1; i <= count; i++)
+  {
+    anser += Increment_Test(1,1);
+  }
+  
+
+  return 0;
+}
+
+int Increment_Test(int x,int y){
+  return x+y;
+}
