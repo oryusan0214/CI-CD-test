3e8000a02402d6dc94940eaf77386127b1215e7e
oryusan0214 <yamada.ryu.rhk@gmail.com>
2023-04-21 17:26:38 +0900

    ADD:ファイル分割のテスト

---
+++ cpp/Test.cpp
@@ -0,0 +1,26 @@
+#include "header/System.h"
+
+int Increment_Test(int x,int y);
+int Decrement_Dest(int x, int y);
+
+int main (){
+  int i;
+  int count;
+  int anser;
+  
+  count=10;
+  for (i = 1; i <= count; i++)
+  {
+    anser += Increment_Test(1,1);
+  }
+  
+
+  return 0;
+}
+
+int Increment_Test(int x,int y){
+  return x+y;
+}
+int Decrement_Test(int x, int y){
+  return x-y;
+}
